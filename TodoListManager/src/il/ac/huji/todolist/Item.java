34e7ee6ebf5f7e094db9d9f6bef455ecd95e9ff2
Kibarski Jonathan <phenixdoc@gmail.com>
2013-03-12 01:08:32 +0200

    Major code cleanup: replaced spaces with tabs

--- TodoListManager/src/il/ac/huji/todolist/Item.java
+++ TodoListManager/src/il/ac/huji/todolist/Item.java
@@ -1,13 +1,13 @@
 package il.ac.huji.todolist;
 
 public class Item {
-	
+
 	private String title = null;
-	
+
 	public Item(String title) {
 		this.title = title;
 	}
-	
+
 	public String toString() {
 		return title;
 	}
--- TodoListManager/src/il/ac/huji/todolist/TodoListManagerActivity.java
+++ TodoListManager/src/il/ac/huji/todolist/TodoListManagerActivity.java
@@ -19,7 +19,6 @@ public class TodoListManagerActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_todo_list_manager);
-
 		ListView listCourses = (ListView) findViewById(R.id.lstTodoItems);
 		List<Item> items = new ArrayList<Item>();
 		adapter = new ItemDisplayAdapter(this, items);
@@ -28,7 +27,6 @@ public class TodoListManagerActivity extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.todo_list_manager, menu);
 		return true;
 	}
